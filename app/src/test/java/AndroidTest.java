1385158318a3aa0a4d99e30637454fce161c2248
Sean Keating <seanjk@hotmail.co.uk>
2016-04-08 22:40:11 +0100

    tests

--- app/src/test/java/AndroidTest.java
+++ app/src/test/java/AndroidTest.java
@@ -1,4 +1,7 @@
-package com.seankeating.focalpoint;
+/**
+ * Created by Sean on 08/04/2016.
+ */
+
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
@@ -6,8 +9,8 @@ import android.test.ApplicationTestCase;
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
+public class AndroidTest extends ApplicationTestCase<Application> {
+    public AndroidTest() {
         super(Application.class);
     }
 }
